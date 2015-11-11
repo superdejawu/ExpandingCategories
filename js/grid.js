47466c12ef0faf69d3aea013a3b824d95f7cea70
George Wu <george@georgewu.ca>
2015-11-11 12:34:40 -0500

    js refactoring

--- js/grid.js
+++ js/grid.js
@@ -9,70 +9,40 @@ $(document).ready(function($) {
 
 	//on Catagory Preview click function
 	preview.on("click",function(){
+			
+			//init height vars
 			var expanderHeight,
 				totalHeight;
 			
-
+			//init parent (category) and sibling (expander) selector
 			var thisCategory = $(this).parent();
 			var thisExpander = $(this).next();
 
-			//Expand or collapse this panel
+			//if expander is expanded
+			if(thisExpander.hasClass('catalog-category-expander-reveal')){
+				//set total category height to preview height
+				totalHeight= previewHeight;
+			}
+
+			//if expander is collapsed
+			else{
+				//calculate total category height
+				expanderHeight = thisExpander.get(0).scrollHeight
+				totalHeight = previewHeight + expanderHeight;
+			}
+
+			//Toggle expand or collapse this panel
 			thisCategory.toggleClass("og-expanded");
 			thisExpander.toggleClass("catalog-category-expander-reveal",300, "easeInOutCubic");
 
-
-
-			if(thisExpander.hasClass('catalog-category-expander-reveal')){
-
-				// $(this).next().slideUp('catalog-category-expander-reveal');
-				
-				// shrink everything else
-				$('.catalog-category').not(thisCategory).animate({height: previewHeight},200);
-
-
-
-				expanderHeight = thisExpander.get(0).scrollHeight;
-
-
-
-				totalHeight= previewHeight;
-				console.log(totalHeight);
-
-
-				
-
-			}
-			else{
-
-
-				// shrink everything else
-				$('.catalog-category').not(thisCategory).animate({height: previewHeight},200);
-
-				// $(this).next().slideDown('catalog-category-expander-reveal');
-				expanderHeight = thisExpander.get(0).scrollHeight
-
-
-
-				totalHeight = previewHeight + expanderHeight;
-				console.log(totalHeight);
-
-
-
-
-
-			}
-
+			//animate category height
    			 thisCategory.animate({height:totalHeight},300, "easeInOutCubic");
-   			 console.log(totalHeight);
 
-
-
-			// thisCategory.css({ height: totalHeight });
-
+   			//Collapse the other panels 
+			$('.catalog-category').not(thisCategory).animate({height: previewHeight},200);
 			//Hide the other panels
 			$(".catalog-category-expander-reveal").not($(this).next()).removeClass('catalog-category-expander-reveal');
-			// $(".catalog-category-expander-reveal").not($(this).next()).css({height: previewHeight});
-
+			//Hide the arrow
 			$(".og-expanded").not(thisCategory).removeClass('og-expanded');
 
 
